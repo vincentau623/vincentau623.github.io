7ce73f80fdc6a44145e113196995a1ad429937ca
Vincent Au <vincent.au@housingauthority.gov.hk>
2022-06-13 16:42:55 +0800

    Add variable

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 
 function App() {
   const [results, setResults] = useState([]);
+  const [times, setTimes] = useState(10);
 
   const generateResult = async () => {
-    const random = [...Array(6)]
+    console.log(times);
+    let randomArr = await Promise.all(
+      [...Array(parseInt(times))].map((el) => {
+        return generate6number();
+      })
+    );
+    console.log("randomArr", randomArr);
+
+    const selection = parseInt(times * Math.random());
+    const random = randomArr[selection];
+
+    if (validation(random)) {
+      // retry
+      generateResult();
+    } else {
+      setResults([...results, random]);
+    }
+  };
+
+  const generate6number = async () => {
+    return [...Array(6)]
       .map((el) => {
         return Math.round(49 * Math.random()).toLocaleString("en-US", {
           minimumIntegerDigits: 2,
@@ -14,35 +37,40 @@ function App() {
         });
       })
       .sort((a, b) => (a > b ? 1 : -1));
-    if (hasDuplicates(random)) {
-      generateResult();
-    } else {
-      setResults([...results, random]);
-    }
   };
+
   const remove = (ind) => {
     if (ind > -1) {
-      const temp = [...results]
-      temp.splice(ind, 1)
+      const temp = [...results];
+      temp.splice(ind, 1);
       setResults(temp);
     }
   };
 
-  function hasDuplicates(arr) {
+  function validation(arr) {
     return new Set(arr).size !== arr.length;
   }
+
+  const handleChange = (event) => {
+    setTimes(event.target.value);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={() => generateResult()}>Learn React</button>
+        <TextField id="outlined-basic" variant="outlined" onChange={handleChange} value={times} color="success"/>
+        <Button variant="contained" onClick={() => generateResult()}>
+          Generate
+        </Button>
         {results && (
           <div style={{ whiteSpace: "pre-line" }}>
             {results.map((el, ind) => {
               return (
                 <div>
                   {`${el}`}
-                  <button onClick={() => remove(ind)}>x</button>
+                  <Button variant="contained" onClick={() => remove(ind)}>
+                    x
+                  </Button>
                 </div>
               );
             })}
